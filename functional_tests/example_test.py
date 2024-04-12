810f632f4c47f03ea7d45f8564e2e4014a80ea43
BognaLew <100587236+BognaLew@users.noreply.github.com>
2024-04-12 14:56:02 +0200

    fixed linter issues

--- functional_tests/example_test.py
+++ functional_tests/example_test.py
@@ -2,17 +2,17 @@ import os
 import unittest
 
 import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from pyvirtualdisplay import Display
 
 class ExampeTest(unittest.TestCase):
     def setUp(self):
-        if os.name == 'posix':
+        if os.name == "posix":
             display = Display(visible=0, size=(800, 800))
             display.start()
             chromedriver_autoinstaller.install()
-        self.BASE_URL = 'http://localhost:8080'
+        self.BASE_URL = "http://localhost:8080"
         self.driver = webdriver.Chrome()
 
     def tearDown(self):
@@ -20,6 +20,4 @@ class ExampeTest(unittest.TestCase):
 
     def test_example(self):
         self.driver.get(self.BASE_URL)
-        t = self.driver.find_element(by=By.XPATH, value='//h1').text
-        assert t == "Hello World!"
-
+        assert self.driver.find_element(by=By.XPATH, value="//h1").text == "Hello World!"
