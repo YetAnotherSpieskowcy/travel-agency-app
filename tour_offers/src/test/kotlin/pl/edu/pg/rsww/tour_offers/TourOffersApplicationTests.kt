2a124c2b907b3196a93c899855375c404be58882
BognaLew <100587236+BognaLew@users.noreply.github.com>
2024-04-19 10:33:22 +0200

    created initial project

---
+++ tour_offers/src/test/kotlin/pl/edu/pg/rsww/tour_offers/TourOffersApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.pg.rsww.tour_offers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TourOffersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
