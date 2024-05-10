8f510e985302b94d77eb1096145658540c78e55e
Jakub Kuczys <me@jacken.men>
2024-05-10 02:27:48 +0200

    Containerize db scripts

--- db_scripts/fill_with_sample_data.sh
+++ db_scripts/fill_with_sample_data.sh
@@ -2,63 +2,41 @@
 
 set -xeo pipefail
 
-MONGO_HOST="${MONGO_HOST:-student-swarm01.maas}"
 MONGO_PORT="${MONGO_PORT:-27017}"
 MONGO_URL="mongodb://$MONGO_USERNAME:$MONGO_PASSWORD@$MONGO_HOST:$MONGO_PORT/"
 
-POSTGRES_HOST="${POSTGRES_HOST:-student-swarm01.maas}"
-POSTGRES_PORT="${POSTGRES_PORT:-5432}"
-POSTGRES_DB="${POSTGRES_DB:-5432}"
+export PGHOST="${POSTGRES_HOST:-student-swarm01.maas}"
+export PGPORT="${POSTGRES_PORT:-5432}"
+export PGDATABASE="${POSTGRES_DB:-5432}"
+export PGUSER="$POSTGRES_USERNAME"
+export PGPASSWORD="$POSTGRES_PASSWORD"
 
-db_scripts=$(realpath db_scripts)
+# Wait for DB servers to be healthy
+db_ready=0
+for _ in {0..5}; do
+    sleep 1
+	if ! mongo --quiet "$MONGO_URL" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; then
+		continue
+	fi
+	if ! pg_isready; then
+	    continue
+	fi
+	db_ready=1
+	break
+done
+if [[ "$db_ready" -eq 0 ]]; then
+	echo 'Failed to connect to DBs'
+	exit 1
+fi
 
-# MongoDB - document schemas
-docker run --rm \
-	--network host \
-	-v "$db_scripts/mongo:/scripts" \
-	-e "MONGO_URL=$MONGO_URL" \
-	mongo:4.4.9 \
-	bash -O nullglob -c 'for file in /scripts/*.js; do mongo "$MONGO_URL" "$file"; done'
+# MongoDB
+find /scripts/mongo -name '*.js' -print0 | xargs -0 -n 1 mongo "$MONGO_URL"
+echo 'MongoDB: Sample data filling finished.'
 
-# MongoDB - samples
-docker run --rm \
-	--network host \
-	-v "$db_scripts/mongo/50_samples:/scripts" \
-	-e "MONGO_URL=$MONGO_URL" \
-	mongo:4.4.9 \
-	bash -O nullglob -c 'for file in /scripts/*.js; do mongo "$MONGO_URL" "$file"; done'
+# PostgreSQL - create database using maintenance db
+PGDATABASE='' psql -v 'ON_ERROR_STOP=1' -f /scripts/sql/00_create_db.sql
+# PostgreSQL - run all SQL scripts
+find /scripts/sql -name '*.sql' -print0 | xargs -0 -n 1 psql -v 'ON_ERROR_STOP=1' -f >/dev/null
+echo 'PostgreSQL: Sample data filling finished.'
 
-# PostgreSQL - create database
-docker run --rm \
-	--network host \
-	-v "$db_scripts/sql:/scripts" \
-	-e "PGUSER=$POSTGRES_USERNAME" \
-	-e "PGPASSWORD=$POSTGRES_PASSWORD" \
-	-e "PGHOST=$POSTGRES_HOST" \
-	-e "PGPORT=$POSTGRES_PORT" \
-	postgres:13 \
-	psql -f /scripts/00_create_db.sql
-
-# PostgreSQL - table schemas
-docker run --rm \
-	--network host \
-	-v "$db_scripts/sql:/scripts" \
-	-e "PGUSER=$POSTGRES_USERNAME" \
-	-e "PGPASSWORD=$POSTGRES_PASSWORD" \
-	-e "PGHOST=$POSTGRES_HOST" \
-	-e "PGPORT=$POSTGRES_PORT" \
-	-e "PGDATABASE=$POSTGRES_DB" \
-	postgres:13 \
-	bash -O nullglob -c 'for file in /scripts/*.sql; do psql -f "$file"; done'
-
-# PostgreSQL - samples
-docker run --rm \
-	--network host \
-	-v "$db_scripts/sql/50_samples:/scripts" \
-	-e "PGUSER=$POSTGRES_USERNAME" \
-	-e "PGPASSWORD=$POSTGRES_PASSWORD" \
-	-e "PGHOST=$POSTGRES_HOST" \
-	-e "PGPORT=$POSTGRES_PORT" \
-	-e "PGDATABASE=$POSTGRES_DB" \
-	postgres:13 \
-	bash -O nullglob -c 'for file in /scripts/*.sql; do psql -f "$file"; done'
+echo '*: Sample data filling finished.'
