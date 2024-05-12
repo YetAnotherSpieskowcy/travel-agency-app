b6cd306c5eb52512fcd33d5dc32275f30caa123b
bstrzele <bstrzelecki25@gmail.com>
2024-05-12 12:28:22 +0200

    functional tests optimalisation

---
+++ functional_tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+@pytest.fixture
+def base_url():
+    port = os.environ.get("PYTEST_PORT")
+    port = port if port is not None else "8080"
+    return f"http://localhost:{port}"
+
+@pytest.fixture
+def driver(base_url):
+    if os.name == "posix":
+        display = Display(visible=0, size=(800, 800))
+        display.start()
+        chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
+    driver.get(base_url)
+
+    yield driver
+
+    driver.close()
--- functional_tests/search_test.py
+++ functional_tests/search_test.py
@@ -1,82 +1,69 @@
 # flake8: noqa E731 not aplicable when working with selenium
-import os
-import unittest
-
-import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 
-class SearchTest(unittest.TestCase):
-    def setUp(self):
-        if os.name == "posix":
-            display = Display(visible=0, size=(800, 800))
-            display.start()
-            chromedriver_autoinstaller.install()
-        self.BASE_URL = "http://localhost:8080"
-        self.driver = webdriver.Chrome()
 
-    def tearDown(self):
-        self.driver.close()
+@pytest.mark.flaky(retries=2, only_on=[TimeoutException])
+def test_search_string(driver):
+    wait = WebDriverWait(driver, 5)
+    results = lambda: driver.find_element(By.ID, "results")
+    assert (  # Test if list is empty
+        results().find_element(By.TAG_NAME, "p").text
+        == "Start typing to see the results..."
+    )
+    search_bar = lambda: driver.find_element(By.NAME, "search")
+    search_bar().send_keys("test search string")
 
-    def test_search_string(self):
-        self.driver.get(self.BASE_URL)
+    wait.until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/div[1]/p[1]"))
+    )
+    assert (  # Test if list contains at least one correct result
+        results().find_element(By.XPATH, "/html/body/div/div/div[1]/div[1]/p[1]").text
+        == "test search string"
+    )
+    assert (  # Test if list contains at least one correct result
+        results().find_element(By.XPATH, "//div/div/p[2]").text == "Hello, world!"
+    )
 
-        results = lambda: self.driver.find_element(By.ID, "results")
-        assert (  # Test if list is empty
-            results().find_element(By.TAG_NAME, "p").text
-            == "Start typing to see the results..."
-        )
-        search_bar = lambda: self.driver.find_element(By.NAME, "search")
-        search_bar().send_keys("test search string")
+@pytest.mark.flaky(retries=2, only_on=[TimeoutException])
+def test_details_view(driver):
+    wait = WebDriverWait(driver, 5)
+    search_bar = lambda: driver.find_element(By.NAME, "search")
+    search_bar().send_keys("test search string")
 
-        assert (  # Test if list contains at least one correct result
-            results().find_element(By.XPATH, "//div/div/p[1]").text
-            == "test search string"
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "/html/body/div/div/div[1]/div[2]/button")
         )
-        assert (  # Test if list contains at least one correct result
-            results().find_element(By.XPATH, "//div/div/p[2]").text == "Hello, world!"
-        )
+    )
+    button = lambda: driver.find_element(
+        By.XPATH, "/html/body/div/div/div[1]/div[2]/button"
+    )
+    assert button().text == "DETAILS"
+    button().click()
 
-    def test_details_view(self):
-        self.driver.get(self.BASE_URL)
-        wait = WebDriverWait(self.driver, 5)
-
-        search_bar = lambda: self.driver.find_element(By.NAME, "search")
-        search_bar().send_keys("test search string")
-
-        wait.until(
-            EC.presence_of_element_located(
-                (By.XPATH, "/html/body/div/div/div[1]/div[2]/button")
-            )
-        )
-        button = lambda: self.driver.find_element(
-            By.XPATH, "/html/body/div/div/div[1]/div[2]/button"
-        )
-        assert button().text == "DETAILS"
-        button().click()
-
-        wait.until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/p[1]"))
-        )
-        assert (
-            self.driver.find_element(By.XPATH, "/html/body/div/div/div[1]/p[1]").text
-            == "Trip no. 0"
-        )
-        assert (
-            self.driver.find_element(By.XPATH, "/html/body/div/div/div[1]/p[2]").text
-            == "A slightly longer description for trip number 0"
-        )
-        cancel = lambda: self.driver.find_element(
-            By.XPATH, "/html/body/div/div/div[1]/button"
-        )
-        assert cancel().text == "CANCEL"
-        cancel().click()
-        results = lambda: self.driver.find_element(By.ID, "results")
-        assert (  # Test if list is empty
-            results().find_element(By.TAG_NAME, "p").text
-            == "Start typing to see the results..."
-        )
+    wait.until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/p[1]"))
+    )
+    assert (
+        driver.find_element(By.XPATH, "/html/body/div/div/div[1]/p[1]").text
+        == "Trip no. 0"
+    )
+    assert (
+        driver.find_element(By.XPATH, "/html/body/div/div/div[1]/p[2]").text
+        == "A slightly longer description for trip number 0"
+    )
+    cancel = lambda: driver.find_element(
+        By.XPATH, "/html/body/div/div/div[1]/button"
+    )
+    assert cancel().text == "CANCEL"
+    cancel().click()
+    results = lambda: driver.find_element(By.ID, "results")
+    assert (  # Test if list is empty
+        results().find_element(By.TAG_NAME, "p").text
+        == "Start typing to see the results..."
+    )
