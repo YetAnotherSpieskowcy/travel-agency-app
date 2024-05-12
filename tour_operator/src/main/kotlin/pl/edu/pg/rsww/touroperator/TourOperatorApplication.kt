90a4c1632f7bcfc11b23373229c0ec832b9c4d86
Bartosz Strzelecki <48790127+bstrzelecki@users.noreply.github.com>
2024-05-12 17:45:06 +0200

    Service for tour operator (billing) (#30)

---
+++ tour_operator/src/main/kotlin/pl/edu/pg/rsww/touroperator/TourOperatorApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.pg.rsww.touroperator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TourOperatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<TourOperatorApplication>(*args)
+}
