075e61aee09e01e4aa191d1371bf762231081b1e
Jakub Kuczys <me@jacken.men>
2024-05-21 04:27:39 +0200

    Add proper error to faililng reservation confirmation (purchase)

--- trip_reservations/src/main/kotlin/pl/edu/pg/rsww/tripreservations/TripReservationServer.kt
+++ trip_reservations/src/main/kotlin/pl/edu/pg/rsww/tripreservations/TripReservationServer.kt
@@ -42,7 +42,16 @@ public class TripReservationServer(
         }
         if (request.path == "/confirm_reservation") {
             if (controller.activeOrchestrators[request.params["sagaId"]] == null) {
-                sendHttpResponse(template, message, """{"success":false}""")
+                sendHttpResponse(
+                    template,
+                    message,
+                    """
+                    <p>The reservation could not be found or has expired.</p>
+                    <button type="button"
+                        class="flex select-none items-center gap-3 rounded-lg border border-gray-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-gray-500 transition-all hover:opacity-75 focus:ring focus:ring-gray-200 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        hx-get="/search.html" hx-target="#container">Go back to tour list</button>
+                    """,
+                )
                 return
             }
 
