1c0a83f7733d3d74f4f284b6972f7fdf66d74938
Jakub Kuczys <me@jacken.men>
2024-06-09 11:15:09 +0200

    Add multiplier to price calculation

--- price_calculator/src/test/kotlin/pl/edu/pg/rsww/pricecalculator/PriceCalculatorTest.kt
+++ price_calculator/src/test/kotlin/pl/edu/pg/rsww/pricecalculator/PriceCalculatorTest.kt
@@ -31,7 +31,7 @@ internal class PriceCalculatorTest {
 
         val expected: Float = 2 * (1513 + 7 * 5.0f * 10 + 250 + 80 + 100 / 2)
 
-        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "flight", 5.0f, "All inclusive"))
+        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "flight", 5.0f, "All inclusive", 1.0f))
     }
 
     @Test
@@ -41,7 +41,7 @@ internal class PriceCalculatorTest {
 
         val expected: Float = 2 * (1513 + 7 * 3.5f * 10 + 100 + 35 + 100 / 2)
 
-        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "bus", 3.5f, "3 posi\u0142ki"))
+        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "bus", 3.5f, "3 posi\u0142ki", 1.0f))
     }
 
     @Test
@@ -51,7 +51,7 @@ internal class PriceCalculatorTest {
 
         val expected: Float = 2 * (1513 + 7 * 4.0f * 10 + 0 + 25 + 100 / 2)
 
-        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "", 4.0f, "2 posi\u0142ki"))
+        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 2, 2, "", 4.0f, "2 posi\u0142ki", 1.0f))
     }
 
     @Test
@@ -61,6 +61,16 @@ internal class PriceCalculatorTest {
 
         val expected: Float = 4 * (1513 + 7 * 5.0f * 10 + 250 + 80 + 100 / 1)
 
-        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 4, 1, "flight", 5.0f, "All inclusive"))
+        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 4, 1, "flight", 5.0f, "All inclusive", 1.0f))
+    }
+
+    @Test
+    fun testCalculatePriceMultiplier() {
+        val fromLocation: Geolocation = Geolocation(54.0f, 18.0f)
+        val destLocation: Geolocation = Geolocation(41.0f, 12.0f)
+
+        val expected: Float = 1.5f * 4 * (1513 + 7 * 5.0f * 10 + 250 + 80 + 100 / 1)
+
+        assertEquals(expected, calculator.calculatePrice(destLocation, fromLocation, 7, 4, 1, "flight", 5.0f, "All inclusive", 1.5f))
     }
 }
