67b50296583eebef3462516998ff22b7d2fa9963
Bartosz Strzelecki <48790127+bstrzelecki@users.noreply.github.com>
2024-06-09 18:20:28 +0200

    Presenting information in real time that offer is currently being viewed (#70)
    * Presenting information in real time on the website that someone else has purchased an offer related to the tour currently being viewed #71

---
+++ status/src/test/kotlin/pl/edu/pg/rsww/status/StatusApplicationTests.kt
@@ -0,0 +1,11 @@
+package pl.edu.pg.rsww.status
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StatusApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
